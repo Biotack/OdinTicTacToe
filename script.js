7114370a2d8927e30213cd76bdc482e5e6af09f5
Biotack <cvelshield@gmail.com>
2023-05-19 20:49:38 -0700

    Initial grid set up and some styling for game board

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const HashBoard = (() => {
-  const gameBoard = [["","","","","","","","",""]]
-  const buildGameBoard = () => {
+  const gameBoard = [[`1`,`2`,``,``,``,``,``,``,``]]
+  
+  const buildBaseGameBoard = () => {
     return gameBoard;
   }
+  const resetGameBoard = () => {
+    gameBoard = [[``,``,``,``,``,``,``,``,``]]
+  }
+  const fillGameBoardHash = (x, y, val) => {
+    gameBoard[x][y] = val;
+  }
+  return {buildBaseGameBoard, resetGameBoard, fillGameBoardHash}
+})();
+
+const initGame = (() => {
+
+})
+
+const playerChar = (() => {
+  const currentPlayer = {}
 })
